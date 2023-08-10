6bf6a8fc261bd5e62e9ef15d5ec214fec551e56d
Colin Campbell <colincampbell321123@hotmail.com>
2023-08-10 22:11:40 +0100

    Restructure adjacent rule as it applies to the grid rather than individual tiles

--- bin/app.pl
+++ bin/app.pl
@@ -4,24 +4,20 @@ use strict;
 use warnings;
 
 use FindBin;
-
-use lib '../lib';
+use FindBin::libs;
 
 use Tents::PuzzleFactory;
 use Tents::Rules::Adjacent;
 
 my $puzzle = Tents::PuzzleFactory::puzzle_from_file( $FindBin::Bin . '/../tents/6x6.json' );
 
+print "Starting puzzle\n\n";
+
 print $puzzle->draw();
 
-print "\nApply adjacent rule\n";
+print "\nApply adjacent rule\n\n";
 
 my $adjacent_rule = Tents::Rules::Adjacent->new( grid => $puzzle->grid );
-
-for ( my $y = 0; $y < scalar( @{ $puzzle->rows } ); $y++ ) {
-    for ( my $x = 0; $x < scalar( @{ $puzzle->columns } ); $x++ ) {
-        $adjacent_rule->apply( $x, $y );
-    }
-}
+$adjacent_rule->apply();
 
 print $puzzle->draw();
--- lib/Tents/Rules/Adjacent.pm
+++ lib/Tents/Rules/Adjacent.pm
@@ -4,19 +4,25 @@ use Moose;
 extends 'Tents::Rules::Rule';
 
 sub apply {
-    my ( $self, $x, $y ) = @_;
+    my $self = shift;
 
-    return if $self->grid->[$y]->[$x] ne '.';
+ROW:
+    for ( my $y = 0; $y < scalar( @{ $self->grid } ); $y++ ) {
+    COLUMN:
+        for ( my $x = 0; $x < scalar( @{ $self->grid->[0] } ); $x++ ) {
+            next if $self->grid->[$y]->[$x] ne '.';
 
-    my $adjacent_tiles = $self->get_adjacent_tiles( $x, $y );
+            my $adjacent_tiles = $self->get_adjacent_tiles( $x, $y );
 
-    foreach my $tile ( @{$adjacent_tiles} ) {
-        if ( defined($tile) && $tile eq 'T' ) {
-            return;
+            foreach my $tile ( @{$adjacent_tiles} ) {
+                if ( defined($tile) && $tile eq 'T' ) {
+                    next COLUMN;
+                }
+            }
+
+            $self->grid->[$y]->[$x] = "G";
         }
     }
-
-    $self->grid->[$y]->[$x] = "G";
 }
 
 1;
