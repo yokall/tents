f73acf24d54cc6c4bce9a5ae9643d2cc9308de87
Colin Campbell <colincampbell321123@hotmail.com>
2023-08-15 21:38:23 +0100

    Implement zero count rule

--- bin/app.pl
+++ bin/app.pl
@@ -8,10 +8,18 @@ use FindBin::libs;
 
 use Tents::PuzzleFactory;
 use Tents::Rules::Adjacent;
+use Tents::Rules::ZeroCounts;
 
 my $puzzle = Tents::PuzzleFactory::puzzle_from_file( $FindBin::Bin . '/../tents/6x6.json' );
 
 print "Starting puzzle\n\n";
+
+print $puzzle->draw();
+
+print "\nApply zero counts rule\n\n";
+
+my $zero_counts_rule = Tents::Rules::ZeroCounts->new( puzzle => $puzzle );
+$zero_counts_rule->apply();
 
 print $puzzle->draw();
 
