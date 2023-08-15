56b90131dfe159c1118f78b3e4929c9054e1f8de
Colin Campbell <colincampbell321123@hotmail.com>
2023-08-15 21:23:42 +0100

    Update test puzzle to have 0 rows and columns

---
+++ t/lib/Test/PuzzleFactory.pm
@@ -0,0 +1,21 @@
+package Test::PuzzleFactory;
+
+use JSON;
+
+use Tents::Puzzle;
+
+sub puzzle_from_file {
+    my $filepath = shift;
+
+    my $data;
+    {
+        local $/;    # Slurp mode to read entire file
+        open my $fh, '<', $filepath;
+        $data = decode_json(<$fh>);
+        close $fh;
+    }
+
+    return Tents::Puzzle->new( row_clues => $data->{row_clues}, column_clues => $data->{column_clues}, grid => $data->{grid} );
+}
+
+1;
